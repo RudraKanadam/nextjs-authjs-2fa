72f56b7c43da63b0548d138f4306428660414bb0
Rudra Kanadam <kanadamrudra@gmail.com>
2024-07-11 22:49:50 +0530

    Global Navbar added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "assets.aceternity.com",
+      "www.prisma.io",
+      "nextjs.org",
+      "next-auth.js.org",
+      "tailwindcss.com",
+      "stripe.com",
+    ],
+  },
+};
 
 export default nextConfig;
